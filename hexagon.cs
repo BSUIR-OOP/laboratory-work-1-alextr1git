40b73173adb06a741e176172aa436e35ddefcc7b
Alexander <dasprooa3@gmail.com>
2022-04-13 18:55:56 +0300

    Solution

---
+++ hexagon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork1
+{
+    class hexagon : Figures 
+    {
+        public hexagon(double x1, double x2, double y1, double y2) : base(x1, x2, y1, y2) { 
+        
+        }
+       
+
+    public override void Print()
+        {
+            Console.WriteLine("Hexagon output");
+            base.Print();
+            
+        }
+    }
+}
